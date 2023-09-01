7b713a8a46158b4c0c8ebdb7002235a7b7c59473
Bristi Maharjan <119603931+bristimaharjan@users.noreply.github.com>
2023-09-01 14:25:01 +0000

    add:structure for student

--- include/admin.h
+++ include/admin.h
@@ -1,5 +1,39 @@
 #ifndef ADMIN_H
-#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student
+{
+    char first_name[20];
+    char last_name[20];
+    int symbol_no[15];
+    char grade[2];
+    int DOB[15];
+    int subject1;
+    int subject2;
+    int subject3;
+    int subject4;
+    int subject5;
+    char grade1;
+    char grade2;
+    char grade3;
+    char grade4;
+    char grade5;
+    float gpa;
+    float gpa1;
+    float gpa2;
+    float gpa3;
+    float gpa4;
+    float gpa5;
+};
+
+void menu();
+void addstudent();
+void viewtrecord();
+void searchs();
+
 
 
 
---
+++ src/admin.c
@@ -0,0 +1,98 @@
+#include "admin.h"
+#include "stdlib.h"
+
+void menu(){
+ int choice;
+ 
+ while(1)
+ {
+
+ 
+    printf("\t\t\t=======STUDENT DATABASE======= ");
+    printf("\n\n\n\t\t\t 1.Add Student Record\n ");
+    printf("\t\t\t 2.View Student Record\n ");
+    printf("\t\t\t 3.Search Student Record\n ");
+    printf("\t\t\t 4.Delete Student Record\n ");
+    printf("\t\t\t 5.Calculate Student Record\n ");
+    printf("\t\t\t 6.Exit\n ");
+    printf("\t\t ------------------------------------------------------\n ");
+    printf("\t\t\t Enter your choice \n ");
+    scanf("%d,&choice");
+    switch(choice){
+      case 1:
+         addstudent();
+        
+      break;
+      case 2:
+       viewtrecord();
+       break;
+      case 3:
+        searchs();
+       break;
+      case 4:
+        delete();
+      break;
+      case 5:
+       calculate();
+      break;
+      case 6:
+       exit(0);
+      break;
+
+
+    }
+ } 
+}
+ void addstudent(){
+  char ch;
+  FILE *file_ptr;
+  
+  
+  struct Student student;
+   file_ptr=fopen("grading_system/app/student_info.txt","r");
+  if(file_ptr==NULL)
+  {
+     printf("\n\t\tFILE DOESN'T EXIST\n");
+     exit(0);
+  }
+  
+  do{
+    printf("\t\t\t =======Add Student student======\n\n\n ");
+    
+    printf("\n\t\t\tEnter first name :");
+    gets(student.first_name);
+    printf("\n\t\t\tEnter last name :");
+    gets(student.last_name);
+    printf("\n\t\t\tEnter faculty :");
+    gets(student.grade);
+    fflush(stdin);
+    printf("\n\t\t\tEnter semester :");
+    scanf("
+    printf("\n\t\t\tEnter  c programming marks :");
+    scanf("%d",&student.cmarks);
+    printf("\n\t\t\tEnter sociology marks :");
+    scanf("%d",&student.scit_marks);
+    printf("\n\t\t\tEnter technical communication marks");
+    scanf("%d",&student.tcmarks);
+    printf("\n\t\t\t-------------------------------\n");
+    fflush(stdin);
+    
+    fread(&student,sizeof(struct read_student),1,file_ptr);
+    {
+  
+    fwrite(&student,sizeof( struct input_student ),1,file_ptr);
+    }
+    printf("\n\t\t\tFILE HAS BEEN SUCCESFULLY ADDED");
+    fclose(file_ptr);
+    printf("\t\t\tDo you want to add another record?(y\n): ");
+    scanf("%s",&ch);
+ 
+
+    
+
+
+  }while(ch=='y'||ch=='Y');
+}
+ 
+
+ 
